6970dcdf398062dcaf366ba82d31e8deaa5b6ef2
chrisverde <christian.verde@live.com>
2015-01-09 10:54:41 +0100

    YOLO first commit

---
+++ templateJson.js
@@ -0,0 +1,96 @@
+{
+    "events": [],
+    "museums": [],
+    "horeca": [
+        {
+            "buildingIDs": [
+                "bam",
+                "gare"
+            ],
+            "coords": {
+                "lat": 50.455122,
+                "lon": 3.952936
+            },
+            "name": "Le Central",
+            "phone": "065/35.35.20",
+            "mobilePhone": "0444/12.34.56",
+            "address": "19, Gd place, 7000 Mons",
+            "web": "",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita ab, neque consequatur dicta suscipit explicabo voluptatibus. Deserunt eos praesentium sit reiciendis tempore vitae non quae, sed sint nemo quasi fuga iure. Quae, delectus. Enim consequatur aperiam fugiat odit velit qui laudantium nulla, explicabo totam veniam eveniet nam cumque ab animi unde temporibus soluta officiis maxime sapiente vitae ad in rerum maiores perferendis! Minima repudiandae quae distinctio id alias, harum cupiditate minus, voluptatibus laborum laudantium ad modi repellendus, ut veritatis non? Animi perspiciatis recusandae, dolorem nisi possimus! Nulla laudantium explicabo ea eligendi maiores vitae architecto, mollitia eveniet voluptatum placeat! Vero dolores voluptatibus totam, cupiditate animi debitis quis necessitatibus praesentium, aliquid qui! Facilis sed soluta hic natus aliquam officia unde officiis ea in dolorum ab quis inventore vel consectetur obcaecati, veniam quidem eos. Quia eum vitae beatae fuga dicta natus impedit, esse ipsa adipisci quis quo aliquam, inventore dolore veniam qui facere?",
+            "budget": "",
+            "outside": true,
+            "inside": true,
+            "openDaysSorting": {
+                "Lundi": {
+                    "opening": [
+                        10,
+                        0
+                    ],
+                    "closing": [
+                        21,
+                        30
+                    ]
+                },
+                "Mardi": {
+                    "opening": [
+                        10,
+                        0
+                    ],
+                    "closing": [
+                        21,
+                        30
+                    ]
+                },
+                "Mercredi": {
+                    "opening": [
+                        10,
+                        0
+                    ],
+                    "closing": [
+                        21,
+                        30
+                    ]
+                },
+                "Jeudi": {
+                    "opening": [
+                        10,
+                        0
+                    ],
+                    "closing": [
+                        21,
+                        30
+                    ]
+                },
+                "Vendredi": {
+                    "opening": [
+                        10,
+                        0
+                    ],
+                    "closing": [
+                        21,
+                        30
+                    ]
+                },
+                "Samedi": {
+                    "opening": [
+                        10,
+                        0
+                    ],
+                    "closing": [
+                        23,
+                        0
+                    ]
+                },
+                "Dimanche": {
+                    "opening": [],
+                    "closing": []
+                }
+            },
+            "openDaysDisplay": [
+                "Lundi - vendredi > 10:00 - 21:30",
+                "Samedi > 10:00 - 23:00"
+            ]
+        }
+    ],
+    "shops": []
+}
